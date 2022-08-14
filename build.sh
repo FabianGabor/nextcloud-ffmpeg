d75d0d613e6921bdb71a63d5039da5ae1596b2f4
Gábor <gabor@primergy.lan>
2022-08-14 14:19:15 +0200

    comment out build command

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker image build -t fabiangabor/nextcloud_ffmpeg:latest --pull .
+#docker image build -t fabiangabor/nextcloud_ffmpeg:latest --pull .
 #docker push fabiangabor/nextcloud_ffmpeg:latest
